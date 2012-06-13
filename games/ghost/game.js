fde624fdc39fa30d87398e7a276dc0a75572cb89
Kimball Germane <krgermane@yahoo.com>
2012-06-12 21:56:07 -0600

    Ghosty.

--- games/ghost/game.js
+++ games/ghost/game.js
@@ -1,5 +1,7 @@
 var util = require('util');
+
 var EventEmitter = require('events').EventEmitter;
+var Map = require( '../../lib/map.js' ).Map;
 
 function Gathering( id, creator ) {
 	EventEmitter.call( this );
@@ -8,8 +10,8 @@ function Gathering( id, creator ) {
 	this.creator = creator;
 	
 	this.order = [];
-	this.indices = {};
-	this.connections = {}
+	this.connections = new Map();
+	this.players = new Map();
 }
 
 util.inherits( Gathering, EventEmitter );
@@ -19,54 +21,80 @@ util.inherits( Gathering, EventEmitter );
 // SEND INFORMATION APPROPRIATELY
 
 Gathering.prototype.addPlayer = function( ws, player_name ) {
-	if( ! this.connections.hasOwnProperty( player_name ) ) {
-		this.broadcast( JSON.stringify({ type: 'add', name: player_name }) );
-		
-		for( var i = 0; i < this.order.length; ++i ) {
-			ws.send( JSON.stringify({ type: 'add', this.order[i] 
-		
-		this.indices[ player_name ] = this.order.length;
-		this.order.push( player_name );
-		this.connections[ player_name ] = ws;
+	if( this.order.length === 0 ) {
+		ws.send( JSON.stringify({ type: 'creator' }) );
 	}
+
+	ws.send( JSON.stringify({ type: 'players', players: this.order }) );
+		
+	this.order.push( player_name );
+	this.connections.set( ws, player_name );
+	this.players.set( player_name, ws );
+		
+	this.broadcast( JSON.stringify({ type: 'add', name: player_name }) );
 }
 
 Gathering.prototype.removePlayer = function( ws ) {
-	var player_name = this.playerNameForConnection( ws );
+	var player_name = this.connections.get( ws ), index = this.indexForPlayerName( player_name );
 	
-	if( player_name ) {
-		this.order.splice( this.indices[ player_name ], 1 );
-		delete this.indices[ player_name ];
-		delete this.connections[ player_name ];
+	this.order.splice( index, 1 );
+	this.connections.delete( ws );
+	this.players.delete( player_name );
 		
-		this.broadcast( JSON.stringify({ type: 'remove', name: player_name }) );
+	this.broadcast( JSON.stringify({ type: 'remove', name: player_name }) );
 		
-		if( this.order.length === 0 ) {
-			this.emit( 'empty' );
-		}
+	if( this.order.length === 0 ) {
+		this.emit( 'empty' );
+	}
+	else if( index === 0 ) {
+		this.creator = this.order[0];
+	
+		this.emit( 'new_creator', this.creator );
+		this.players.get( this.creator ).send( JSON.stringify({ type: 'creator' }) );
 	}
 }
 
-Gathering.prototype.playerNameForConnection = function( ws ) {
-	for( var player_name in this.connections ) {
-		if( this.connections.hasOwnProperty( player_name ) ) {
-			if( this.connections[ player_name ] === ws ) {
-				return player_name;
-			}
+Gathering.prototype.indexForPlayerName = function( player_name ) {
+	for( var i = 0; i < this.order.length; ++i ) {
+		if( this.order[i] === player_name ) {
+			return i;
 		}
 	}
 }
 
 Gathering.prototype.broadcast = function( message ) {
-	for( var player_name in this.players ) {
-		if( this.players.hasOwnProperty( player_name ) ) {
-			this.players[ player_name ].send( message );
-		}
-	}
+	this.connections.eachKey( function( ws ) {
+		ws.send( message );
+	});
 }
 
 exports.Gathering = Gathering;
 
 function Game() {}
+
+
+
+Game.prototype.canContinueWithoutPlayer = function( player ) {
+
+}
+
+Game.prototype.removePlayer = function( player ) {
+	if( this.canContinueWithoutPlayer( player ) ) {
+		// remove the player, make changes, etc.
+	}
+	else {
+		// kick everyone off
+	}
+}
+
+Game.prototype.nextTurn = function() {
+	// notify players that it is the next person's turn
+	// set a timeout to drop the player
+}
+
+// the game may be able to be suspended (the game will not be removed from the 
+// system if all the players leave.)
+// when a player reconnects to the game, they receive the game state
+
 
 exports.Game = Game;
--- main.js
+++ main.js
@@ -2,9 +2,14 @@ var fs = require( 'fs' );
 
 exports.handle = function( user_info, response ) {
 	fs.readdir( 'games', function( error, entries ) {
-		entries.forEach( function( entry ) {
-			response.write( '<a href="/' + entry + '">' + entry + '</a><br/>' );
-		});
+		if( error ) {
+		
+		}
+		else {
+			entries.forEach( function( entry ) {
+				response.write( '<a href="/' + entry + '">' + entry + '</a><br/>' );
+			});
+		}
 
 		response.end();
 	});
